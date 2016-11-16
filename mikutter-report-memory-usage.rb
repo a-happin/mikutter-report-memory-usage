93b20066cb5c7284b871e82275fff162d4cf6be7
a-happin <10003601+ankotonoha@users.noreply.github.com>
2016-11-16 19:30:56 +0900

    fix bug('-'v)
    現在の（ツイート間隔）秒前のメモリ使用量が報告されるバグを修正
    ついでにリファクタリング

--- mikutter-report-memory-usage.rb
+++ mikutter-report-memory-usage.rb
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 
 Plugin.create :"mikutter-report-memory-usage" do
-  def show_memory_usage ()
+  def report_memory_usage ()
     usage = `ps -o rss= -p #{$$}`.to_i
-    Reserver.new (UserConfig[:"report-memory-usage-interval"]) {
+    if usage < 1024 then
+      str = sprintf("%dKB", usage)
+    else
+      usage /= 1024.0
       if usage < 1024 then
-        str = sprintf("%dKB", usage)
+        str = sprintf("%.2fMB", usage)
       else
-        usage = usage.fdiv(1024)
-        if usage < 1024 then
-          str = sprintf("%.2fMB", usage)
-        else
-          usage = usage.fdiv(1024)
-          str = sprintf("%.2fGB", usage)
-        end
+        usage /= 1024.0
+        str = sprintf("%.2fGB", usage)
       end
-      activity :system, "mikutterのメモリ使用量: " + str
-      show_memory_usage
+    end
+    activity :system, "mikutterのメモリ使用量: " + str
+    Reserver.new (UserConfig[:"report-memory-usage-interval"]) {
+      report_memory_usage
     }
   end
 
   on_boot do |service|
     UserConfig[:"report-memory-usage-interval"] ||= 300
-    show_memory_usage
+    report_memory_usage
   end
 
   settings "メモリ使用量報告" do
